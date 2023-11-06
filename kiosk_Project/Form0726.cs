e373014c6daa26d7ee5910e7d1fdaa13c967a2f8
sohyunchul <144973910+sohyunchul@users.noreply.github.com>
2023-11-06 11:07:29 +0900

    Add files via upload
    c# WinForm기반 Kiosk_project 수정

---
+++ kiosk_Project/Form0726.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.ComponentModel.Com2Interop;
+
+namespace kioskProject
+{
+    public partial class Form0726 : Form
+    {
+        List<string> labels = new List<string>();
+        List<string> plabels = new List<string>();
+        int ICnt = 0; // 장바구니 칸
+        public int sum; //음료가격
+        public Form0726_1 form0726_1;
+
+        /// <summary>
+        /// 현재 장바구니 1개당 변수 1개의 가격씩 따로 담았던 것을 
+        /// 밑에 int 배열을 통해서 일괄처리 하는 코드로 바꿀 것 
+        /// </summary>
+        int[] basket = new int[3]; // 장바구니 제품 당 가격을 담을 배열
+        int one, two, three; // 장바구니 제품 당 가격을 담을 변수들 
+
+        public Form0726()
+        {
+            InitializeComponent();
+            // 상품명 저장, LabelcoffeeName
+            labels.Add(lbCoffeeN.Text);
+            labels.Add(lbCoffeeN1.Text);
+            labels.Add(lbCoffeeN2.Text);
+            labels.Add(lbCoffeeN3.Text);
+            labels.Add(lbCoffeeN4.Text);
+            labels.Add(lbCoffeeN5.Text);
+            labels.Add(lbCoffeeN6.Text);
+            labels.Add(lbCoffeeN7.Text);
+
+            // 상품가격 저장, Labelcoffeeprice
+            plabels.Add(lbCoffeeP.Text);
+            plabels.Add(lbCoffeeP1.Text);
+            plabels.Add(lbCoffeeP2.Text);
+            plabels.Add(lbCoffeeP3.Text);
+            plabels.Add(lbCoffeeP4.Text);
+            plabels.Add(lbCoffeeP5.Text);
+            plabels.Add(lbCoffeeP6.Text);
+            plabels.Add(lbCoffeeP7.Text);
+
+            // 장바구니
+            pbCal.Image = imageList1.Images[8];
+            pbCal1.Image = imageList1.Images[8];
+            pbCal2.Image = imageList1.Images[8];
+        }
+
+
+        // 키오스크 메인폼
+        private void Form0726_Load(object sender, EventArgs e) {
+            // 초기 탭 페이지 추가
+            TabPage tbCoffee = new TabPage(); // 커피 탭 페이지
+            TabPage tpNonCoffee = new TabPage(); // 음료 탭 페이지
+
+            //탭 컨트롤에 탭페이지 추가
+            tabControl1.TabPages.Add(tbCoffee);
+            tabControl1.TabPages.Add(tpNonCoffee);
+        }
+
+
+        private void pbCoffee_Click(object sender, EventArgs e) {
+            if (ICnt < 3)
+            {
+                if (sender is PictureBox pb)
+                {
+                    switch (pb.Name)
+                    {
+                        case "pbCoffee": form0726_1  = new Form0726_1(imageList1, 0, labels, plabels); break;
+                        case "pbCoffee2": form0726_1 = new Form0726_1(imageList1, 1, labels, plabels); break;
+                        case "pbCoffee3": form0726_1 = new Form0726_1(imageList1, 2, labels, plabels); break;
+                        case "pbCoffee4": form0726_1 = new Form0726_1(imageList1, 3, labels, plabels); break;
+                        case "pbCoffee5": form0726_1 = new Form0726_1(imageList1, 4, labels, plabels); break;
+                        case "pbCoffee6": form0726_1 = new Form0726_1(imageList1, 5, labels, plabels); break;
+                        case "pbCoffee7": form0726_1 = new Form0726_1(imageList1, 6, labels, plabels); break;
+                        case "pbCoffee8": form0726_1 = new Form0726_1(imageList1, 7, labels, plabels); break;
+                    }
+                    
+                    form0726_1.SendCompleted += new Form0726_1.SendToName(SettingName);
+                    form0726_1.sendPrice += new Form0726_1.SendClac(SettingPrice);
+                    form0726_1.ShowDialog();
+                }
+            }
+            else
+            {
+                MessageBox.Show("장바구니가 꽉 찼습니다.");
+                return;
+            }
+        }
+
+        // 커피(Coffee) 메뉴화면 이전버튼
+        private void btnBack_Click(object sender, EventArgs e) 
+        {
+            if (tabControl1.SelectedIndex < tabControl1.TabCount + 1)
+            {
+                tabControl1.SelectedIndex -= 1;
+            }
+        }
+
+        // 커피(Coffee) 메뉴화면 다음버튼
+        private void btnNext_Click(object sender, EventArgs e) 
+        {
+            if (tabControl1.SelectedIndex < tabControl1.TabCount - 1)
+            {
+                tabControl1.SelectedIndex += 1;
+            }
+        }
+
+        //음료화면 이전버튼
+        private void btnBack1_Click(object sender, EventArgs e) 
+        {
+            if (tabControl1.SelectedIndex < tabControl1.TabCount + 1)
+            {
+                tabControl1.SelectedIndex -= 1;
+            }
+        }
+
+        // 전체취소버튼
+        private void btnCancel_Click(object sender, EventArgs e) 
+        {
+            ICnt = 0;
+            one = 0;
+            two = 0;
+            three = 0;
+            pbCal .Image = imageList1.Images[8];
+            pbCal1.Image = imageList1.Images[8];
+            pbCal2.Image = imageList1.Images[8];
+            lbCal  .Text = "";
+            lbName .Text = "";
+            lbName1.Text = "";
+            lbName2.Text = "";
+        }
+
+        // 결제버튼
+        private void btnCal_Click(object sender, EventArgs e)  
+        {
+            if (ICnt == 0)
+            {
+                MessageBox.Show("장바구니에 음료를 담아주세요");
+            }
+            else
+            {
+                MessageBox.Show("결제가 완료되었습니다.");
+                this.Close();
+            }
+        }
+
+        // 장바구니에 담기는 가격, 상품명, 이미지
+        public void SettingName(string x, int idx, int _price)
+        {                   
+            if (ICnt == 0)
+            {
+                one = _price;
+                lbName.Text = x; // 장바구니 이름
+                pbCal.Image = imageList1.Images[idx]; //장바구니 이미지
+            }
+
+            else if (ICnt == 1)
+            {
+                two = _price;
+                lbName1.Text = x; // 장바구니 이름
+                pbCal1.Image = imageList1.Images[idx];
+            }
+
+            else if (ICnt == 2)
+            {
+                three = _price;
+                lbName2.Text = x; // 장바구니 이름
+                pbCal2.Image = imageList1.Images[idx];
+            }                       
+
+            ICnt += 1;            
+            lbCal.Text = string.Format($"\\{one + two + three}");            
+        }       
+
+        // 선택메뉴 최종가격을 메인으로 가져오는 함수
+        private void SettingPrice(int price)
+        {
+            lbCal.Text = price.ToString();
+        }
+    }
+}
---
+++ kiosk_Project/Form0726_1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Runtime.Intrinsics.X86;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kioskProject
+{
+    public partial class Form0726_1 : Form {
+        public delegate void SendToName(string _name, int idx, int price); // 이름, 이미지, 가격        
+        public delegate void SendClac(int price);
+        public event SendToName SendCompleted;
+        public event SendClac sendPrice;
+        public int Iidx;
+        public int ISave_Price;
+        public int cnt = 1;
+        public int originalValue; // shot 수량을 담을 변수
+        public int totalPrice; // shot 총 가격
+
+        string a; // 상품확인 화면 초기값
+        int price = 0; //price  (상품 가격 메인폼과 연동하여 출력)
+        int originalPrice = 0; // 원래 가격을 갖고 있는 변수
+        public Form0726_1(ImageList i, int index, List<string> l, List<string> p) {
+            InitializeComponent();
+            imageList2 = i;
+            Iidx = index;
+            pbItem1.Image = imageList2.Images[index]; // 메인폼에서 상품선택 시 동일 이미지 출력
+
+            lbItem1.Text = l[index]; //labels (상품명 메인폼과 연동하여 출력)
+
+            string[] arr = p[index].Split(',');
+            string[] t = arr[0].Split("\\");
+            a += t[1];
+            for (int j = 1; j < arr.Length; j++)
+                a += arr[j];
+            lbPrice.Text = "\\" + a;
+
+            originalValue = int.Parse(numericUpDown.Value.ToString());
+            
+            price = int.Parse(a);
+            originalPrice = int.Parse(a);
+        }
+
+        // 용도 : 최종 금액을 넘기는 것
+        // 확인버튼 눌렀을 때 주문확인창 닫히고 저장된 상품 메인화면 장바구니에 담길수 있게
+        private void btnOk_Click(object sender, EventArgs e) {
+            string[] arr = lbPrice.Text.Split('\\');
+            sendPrice(int.Parse(arr[1]));
+            SendCompleted(lbItem1.Text, Iidx, int.Parse(arr[1]));            
+            this.Close();
+        }
+
+        // 용도 : lbPrice 가 샷추가된 금액으로 바꾸기 위함
+        private void numericUpDown_ValueChanged(object sender, EventArgs e) {
+            lbPrice.Text = "\\" + MenuAcountCalcuration().ToString();
+        }
+                
+        // (이전 / 이후) Shot 수량에 대한 금액 계산 함수
+        private int MenuAcountCalcuration() {
+            int afterValue = int.Parse(numericUpDown.Value.ToString());
+
+            string[] arr = lbshot.Text.Split("\\");
+            string[] arr2 = lbPrice.Text.Split("\\");
+            int shotPrice = int.Parse(arr[1]);
+                        
+            // 이전 수량 보다 클 경우
+            if (originalValue < afterValue )
+            {
+                totalPrice = int.Parse(arr2[1]) + shotPrice;
+            }
+            // 이전 수량 보다 작을 경우
+            else if (originalValue > afterValue)
+            {
+                totalPrice = int.Parse(arr2[1]) - shotPrice;
+            }
+
+            originalValue = afterValue;
+            
+            return totalPrice;
+        }
+       
+        // 제품 수량 
+        private void btn_Count_Click(object sender, EventArgs e) {
+            if (cnt > 0)
+            {
+                 cnt = int.Parse(lb_Cnt.Text);
+
+                 string[] arr = lbPrice.Text.Split("\\");
+
+                 if (sender is Button btn)
+                 {
+                     switch (btn.Name)
+                     {
+                         case "btn_Plus":
+                             cnt += 1;
+                             lbPrice.Text = "\\" + (int.Parse(arr[1]) + originalPrice).ToString();
+                             break;
+                         case "btn_Minus":
+                            if (cnt == 1)
+                            {
+                                break;
+                            }                                                            
+                              cnt -= 1;
+                              lbPrice.Text = "\\" + (int.Parse(arr[1]) - originalPrice).ToString();                            
+                             break;
+                     }                    
+                 }                                   
+                 lb_Cnt.Text = cnt.ToString();             
+            }
+        }
+    }
+}
+
